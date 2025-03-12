7e487716a4b2325a6c172f54aceb87d36c80c129
Oualid <oualidboudemagh22@gmail.com>
2025-03-12 21:42:19 +0100

    Final

--- Pokemon/pokemon.cpp
+++ Pokemon/pokemon.cpp
@@ -36,16 +36,16 @@ void Pokemon::initializeMoves() {
     }
     //Arceus 
     else if (nom == "Arceus") {
-        spes.push_back(move("Judgment", 100, 100, "Normal","attacks/Hydro-Pump.png"));
-        spes.push_back(move("Hyper Beam", 150, 90, "Normal","attacks/Hydro-Pump.png"));
-        spes.push_back(move("Cosmic Power", 0, 100, "Psychic","attacks/Hydro-Pump.png"));
-        spes.push_back(move("Recover", 0, 100, "Normal","attacks/Hydro-Pump.png"));
+        spes.push_back(move("Judgment", 100, 100, "Normal","attacks/Fissure.png"));
+        spes.push_back(move("Hyper Beam", 150, 90, "Normal","attacks/Giga-impact.png"));
+        spes.push_back(move("Cosmic Power", 100, 100, "Psychic","attacks/.png"));
+        spes.push_back(move("LeafStorm", 100, 100, "Normal","attacks/Leaf-Storm.png"));
     }
      // Charizard
     else if (nom == "Charizard") {
         spes.push_back(move("Flamethrower", 90, 100, "Fire", "attacks/Fire-Punch.png"));
         spes.push_back(move("Dragon Claw", 80, 100, "Dragon", "attacks/Dragon-Claw.png"));
-        spes.push_back(move("Air Slash", 75, 95, "Flying", "attacks/Aerial-Ace.png"));
+        spes.push_back(move("Air Slash", 75, 95, "Flying", "attacks/Iron-Head.png"));
         spes.push_back(move("Solar Beam", 120, 100, "Grass", "attacks/Cut.png"));
     }
     // Mewtwo
@@ -60,98 +60,98 @@ void Pokemon::initializeMoves() {
         spes.push_back(move("Blue Flare", 130, 85, "Fire", "attacks/Sacred-Fire.png"));
         spes.push_back(move("Dragon Claw", 85, 100, "Dragon", "attacks/Dragon-Claw.png"));
         spes.push_back(move("Fusion Flare", 100, 100, "Fire", "attacks/Fire-Punch.png"));
-        spes.push_back(move("Srone Edge", 100, 80, "Rock", "attacks/Srone-Edge.png"));
+        spes.push_back(move("Srone Edge", 100, 80, "Rock", "attacks/Ancient-Power.png"));
     }
     // Zekrom
     else if (nom == "Zekrom") {
-        spes.push_back(move("Bolt Strike", 130, 85, "Electric", "attacks/Cut.png"));
+        spes.push_back(move("Bolt Strike", 130, 85, "Electric", "attacks/Flash-Cannon.png"));
         spes.push_back(move("Dragon Claw", 80, 100, "Dragon", "attacks/Dragon-Claw.png"));
-        spes.push_back(move("Fusion Bolt", 100, 100, "Electric", "attacks/Speed-Attack-Back.png"));
-        spes.push_back(move("Srone Edge", 100, 80, "Rock", "attacks/Srone-Edge.png"));
+        spes.push_back(move("Fusion Bolt", 100, 100, "Electric", "attacks/Flash-Cannon.png"));
+        spes.push_back(move("Srone Edge", 100, 80, "Rock", "attacks/Ancient-Power.png"));
     }
     // Rayquaza
     else if (nom == "Rayquaza") {
-        spes.push_back(move("Dragon Ascent", 120, 100, "Flying", "attacks/Dragon-Ascent.png"));
+        spes.push_back(move("Dragon Ascent", 120, 100, "Flying", "attacks/Iron-Head.png"));
         spes.push_back(move("Dragon Claw", 80, 100, "Dragon", "attacks/Dragon-Claw.png"));
         spes.push_back(move("Extreme Speed", 80, 100, "Normal", "attacks/Extreme-Speed.png"));
         spes.push_back(move("Hyper Beam", 150, 90, "Normal", "attacks/Hyper-Beam.png"));
     }
     // Lugia
     else if (nom == "Lugia") {
-        spes.push_back(move("Aeroblast", 100, 95, "Flying", "attacks/Aeroblast.png"));
+        spes.push_back(move("Aeroblast", 100, 95, "Flying", "attacks/Iron-Head.png"));
         spes.push_back(move("Psychic", 90, 100, "Psychic", "attacks/Psychic.png"));
         spes.push_back(move("Hydro Pump", 110, 80, "Water", "attacks/Hydro-Pump.png"));
-        spes.push_back(move("Recover", 0, 100, "Normal", "attacks/Recover.png"));
+        spes.push_back(move("Leaf Storm", 100, 100, "Normal", "attacks/Leaf-Storm.png"));
     }
     // Zapdos
     else if (nom == "Zapdos") {
-        spes.push_back(move("Thunderbolt", 90, 100, "Electric", "attacks/Thunderbolt.png"));
-        spes.push_back(move("Drill Peck", 80, 100, "Flying", "attacks/Drill-Peck.png"));
-        spes.push_back(move("Heat Wave", 95, 90, "Fire", "attacks/Heat-Wave.png"));
-        spes.push_back(move("Roost", 0, 100, "Flying", "attacks/Roost.png"));
+        spes.push_back(move("Thunderbolt", 90, 100, "Electric", "attacks/Flash-Cannon.png"));
+        spes.push_back(move("Drill Peck", 80, 100, "Flying", "attacks/Iron-Head.png"));
+        spes.push_back(move("Heat Wave", 95, 90, "Fire", "attacks/Fire-Punch.png"));
+        spes.push_back(move("Roost", 0, 100, "Flying", "attacks/Iron-Head.png"));
     }
     // Moltres
     else if (nom == "Moltres") {
-        spes.push_back(move("Fire Blast", 110, 85, "Fire", "attacks/Fire-Blast.png"));
-        spes.push_back(move("Air Slash", 75, 95, "Flying", "attacks/Air-Slash.png"));
-        spes.push_back(move("Solar Beam", 120, 100, "Grass", "attacks/Solar-Beam.png"));
-        spes.push_back(move("Roost", 0, 100, "Flying", "attacks/Roost.png"));
+        spes.push_back(move("Fire Blast", 110, 85, "Fire", "attacks/Fire-Punch.png"));
+        spes.push_back(move("Air Slash", 75, 95, "Flying", "attacks/Iron-Head.png"));
+        spes.push_back(move("Solar Beam", 120, 100, "Grass", "attacks/Cut.png"));
+        spes.push_back(move("Roost", 0, 100, "Flying", "attacks/Iron-Head.png"));
     }
     // Raikou
     else if (nom == "Raikou") {
-        spes.push_back(move("Thunderbolt", 90, 100, "Electric", "attacks/Thunderbolt.png"));
+        spes.push_back(move("Thunderbolt", 90, 100, "Electric", "attacks/Flash-Cannon.png"));
         spes.push_back(move("Shadow Ball", 80, 100, "Ghost", "attacks/Shadow-Ball.png"));
         spes.push_back(move("Extreme Speed", 80, 100, "Normal", "attacks/Extreme-Speed.png"));
-        spes.push_back(move("Calm Mind", 0, 100, "Psychic", "attacks/Calm-Mind.png"));
+        spes.push_back(move("Calm Mind", 0, 100, "Psychic", "attacks/Psychic.png"));
     }
     // Entei
     else if (nom == "Entei") {
         spes.push_back(move("Sacred Fire", 100, 95, "Fire", "attacks/Sacred-Fire.png"));
-        spes.push_back(move("Stone Edge", 100, 80, "Rock", "attacks/Stone-Edge.png"));
+        spes.push_back(move("Stone Edge", 100, 80, "Rock", "attacks/Ancient-Power.png"));
         spes.push_back(move("Extreme Speed", 80, 100, "Normal", "attacks/Extreme-Speed.png"));
         spes.push_back(move("Iron Head", 80, 100, "Steel", "attacks/Iron-Head.png"));
     }
     // Suicune
     else if (nom == "Suicune") {
         spes.push_back(move("Hydro Pump", 110, 80, "Water", "attacks/Hydro-Pump.png"));
-        spes.push_back(move("Ice Beam", 90, 100, "Ice", "attacks/Ice-Beam.png"));
-        spes.push_back(move("Calm Mind", 0, 100, "Psychic", "attacks/Calm-Mind.png"));
-        spes.push_back(move("Mirror Coat", 0, 100, "Psychic", "attacks/Mirror-Coat.png"));
+        spes.push_back(move("Ice Beam", 90, 100, "Ice", "attacks/Surf-Transparent.png"));
+        spes.push_back(move("Calm Mind", 0, 100, "Psychic", "attacks/Psychic.png"));
+        spes.push_back(move("Mirror Coat", 0, 100, "Psychic", "attacks/Psychic.png"));
     }
     // Kyogre
     else if (nom == "Kyogre") {
         spes.push_back(move("Water Spout", 150, 100, "Water", "attacks/Water-Spout.png"));
-        spes.push_back(move("Thunder", 110, 70, "Electric", "attacks/Thunder.png"));
-        spes.push_back(move("Ice Beam", 90, 100, "Ice", "attacks/Ice-Beam.png"));
-        spes.push_back(move("Calm Mind", 0, 100, "Psychic", "attacks/Calm-Mind.png"));
+        spes.push_back(move("Thunder", 110, 70, "Electric", "attacks/Flash-Cannon.png"));
+        spes.push_back(move("Ice Beam", 90, 100, "Ice", "attacks/Surf-Transparent.png"));
+        spes.push_back(move("Calm Mind", 0, 100, "Psychic", "attacks/Psychic.png"));
     }
     // Groudon
     else if (nom == "Groudon") {
-        spes.push_back(move("Precipice Blades", 120, 85, "Ground", "attacks/Precipice-Blades.png"));
+        spes.push_back(move("Precipice Blades", 120, 85, "Ground", "attacks/Rock-Tomb.png"));
         spes.push_back(move("Fire Punch", 75, 100, "Fire", "attacks/Fire-Punch.png"));
-        spes.push_back(move("Stone Edge", 100, 80, "Rock", "attacks/Stone-Edge.png"));
-        spes.push_back(move("Solar Beam", 120, 100, "Grass", "attacks/Solar-Beam.png"));
+        spes.push_back(move("Stone Edge", 100, 80, "Rock", "attacks/Ancient-Power.png"));
+        spes.push_back(move("Solar Beam", 120, 100, "Grass", "attacks/Cut.png"));
     }
     // Ho-Oh
     else if (nom == "Ho-Oh") {
         spes.push_back(move("Sacred Fire", 100, 95, "Fire", "attacks/Sacred-Fire.png"));
-        spes.push_back(move("Brave Bird", 120, 100, "Flying", "attacks/Brave-Bird.png"));
-        spes.push_back(move("Earthquake", 100, 100, "Ground", "attacks/Earthquake.png"));
-        spes.push_back(move("Recover", 0, 100, "Normal", "attacks/Recover.png"));
+        spes.push_back(move("Brave Bird", 120, 100, "Flying", "attacks/Iron-Head.png"));
+        spes.push_back(move("Earthquake", 100, 100, "Ground", "attacks/Rock-Tomb.png"));
+        spes.push_back(move("Leaf Storm", 0, 100, "Normal", "attacks/Leaf-Storm.png"));
     }
     // Articuno
     else if (nom == "Articuno") {
         spes.push_back(move("Blizzard", 110, 70, "Ice", "attacks/Blizzard.png"));
-        spes.push_back(move("Ice Beam", 90, 100, "Ice", "attacks/Ice-Beam.png"));
-        spes.push_back(move("Hurricane", 110, 70, "Flying", "attacks/Hurricane.png"));
-        spes.push_back(move("Roost", 0, 100, "Flying", "attacks/Roost.png"));
+        spes.push_back(move("Ice Beam", 90, 100, "Ice", "attacks/Surf-Transparent.png"));
+        spes.push_back(move("Hurricane", 110, 70, "Flying", "attacks/Iron-Head.png"));
+        spes.push_back(move("Roost", 0, 100, "Flying", "attacks/Iron-Head.png"));
     }
     // Blastoise
     else if (nom == "Blastoise") {
         spes.push_back(move("Hydro Pump", 110, 80, "Water", "attacks/Hydro-Pump.png"));
-        spes.push_back(move("Ice Beam", 90, 100, "Ice", "attacks/Ice-Beam.png"));
+        spes.push_back(move("Ice Beam", 90, 100, "Ice", "attacks/Surf-Transparent.png"));
         spes.push_back(move("Flash Cannon", 80, 100, "Steel", "attacks/Flash-Cannon.png"));
-        spes.push_back(move("Earthquake", 100, 100, "Ground", "attacks/Earthquake.png"));
+        spes.push_back(move("Earthquake", 100, 100, "Ground", "attacks/Rock-Tomb.png"));
     }
 }
     
