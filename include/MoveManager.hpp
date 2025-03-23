62b69189e3fd9dcd866adcfe43dde694f38c7725
21107771 <mathis.renard@etu-sorbonne.universite.fr>
2025-03-23 21:26:47 +0100

    test

--- include/MoveManager.hpp
+++ include/MoveManager.hpp
@@ -1,55 +1,7 @@
 #pragma once
 #include "Move.hpp"
 
-// Définition des mouvements
-inline const Move Flamethrower("Flamethrower", Type::Fire, 90, 100,0);
-inline const Move Fire_Blast("Fire Blast",Type::Fire,110,85,0);
-inline const Move Fusion_Flare("Fusion Flare",Type::Fire,100,100,0);
-inline const Move Fly("Fly", Type::Flying, 90, 95,1);
-inline const Move DragonClaw("Dragon Claw", Type::Dragon, 80, 95,1);
-inline const Move Slash("Slash", Type::Normal, 70, 100,1);
-inline const Move Judgment("Judgment",Type::Normal,100,100,0);
-inline const Move Earth_Power("Earth Power",Type::Ground,90,100,0);
-inline const Move Gunk_Shot("Gunk Shot",Type::Poison,120,80,1);
-inline const Move Psychics("Psychics",Type::Psychic,90,100,0);
-inline const Move Thunderbolt("Thunderbolt",Type::Electric,90,100,0);
-inline const Move Thunder("Thunder",Type::Electric,110,70,0);
-inline const Move Ice_Beam("Ice Beam",Type::Ice,90,100,0);
-inline const Move Sludge_Bomb("Sludge Bomb",Type::Poison,90,100,0);
-inline const Move Bug_Buzz("Bug Buzz",Type::Bug,90,100,0);
-inline const Move Dragon_Pulse("Dragon Pulse",Type::Dragon,85,100,0);
-inline const Move Draco_Meteor("Draco Meteor",Type::Dragon,130,70,0);
-inline const Move Moonblast("Moonblast",Type::Fairy,95,100,0);
-inline const Move Flash_Cannon("Flash Cannon",Type::Steel,80,100,0);
-inline const Move Energy_Ball("Energy Ball",Type::Grass,90,100,0);
-inline const Move Fiery_Wrath("Fiery Wrath",Type::Dark,90,100,0);
-inline const Move Scald("Scald",Type::Water,80,100,0);
-inline const Move Origin_Pulse("Origin Pulse",Type::Water,110,85,0);  
-inline const Move Shadow_Ball("Shadow Ball",Type::Ghost,80,100,0);
-inline const Move Aura_Sphere("Aura Sphere",Type::Fighting,80,100,0);
-inline const Move Aeroblast("Aeroblast",Type::Flying,100,95,0);
-inline const Move Hurricane("Hurricane",Type::Flying,110,70,0);
-inline const Move Liquidation("Liquidation",Type::Water,85,100,1);
-inline const Move Iron_Head("Iron Head",Type::Steel,80,100,1);
-inline const Move Iron_Tail("Iron Tail",Type::Steel,100,75,1);
-inline const Move Stone_Edge("Stone Edge",Type::Rock,100,90,1);
-inline const Move Power_Gem("Power_Gem",Type::Rock,80,100,0);
-inline const Move Psyblade("Psyblade",Type::Psychic,80,100,1);
-inline const Move Poison_Jab("Poison Jab",Type::Poison,80,100,1);
-inline const Move Ice_Spinner("Ice Spinner",Type::Ice,80,100,1);
-inline const Move Lands_Wrath("Land's Wrath",Type::Ground,90,100,1);
-inline const Move Precipice_Blades("Precipice Blades",Type::Ground,120,85,1);
-inline const Move Leaf_Blade("Leaf Blade",Type::Grass,90,100,1);
-inline const Move Poltergeist("Poltergeist",Type::Ghost,110,90,1);
-inline const Move Sacred_Fire("Sacred Fire",Type::Fire,100,95,1);
-inline const Move Fire_Punch("Fire Punch",Type::Fire,75,100,1);
-inline const Move Focus_Blast("Focus Blast",Type::Fighting,120,70,0);
-inline const Move Sacred_Sword("Sacred Sword",Type::Fighting,90,100,1);
-inline const Move Play_Rough("Play Rough",Type::Fairy,90,90,1);
-inline const Move Fusion_Bolt("Fusion Bolt",Type::Electric,100,100,1);
-inline const Move Wild_Charge("Wild Charge",Type::Electric,90,100,1);
-inline const Move Foul_Play("Foul Play",Type::Dark,95,100,1);
-inline const Move Lunge("Lunge",Type::Bug,80,100,1);
+
 // Déclaration des mouvements
 
 // Normal
